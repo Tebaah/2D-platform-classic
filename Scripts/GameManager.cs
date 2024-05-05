927cd4a28bf727af5c486a691db47934b92e6e98
Tebaah <e2nd@protonmail.com>
2024-05-05 00:24:55 -0400

    feat(menu):level selection and startup menu completed

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -5,32 +5,29 @@ public partial class GameManager : Node
 {
     // variable global
     private Global _global;
+
+    // variable para instanciar al jugador
     public PackedScene player = GD.Load<PackedScene>("res://Scenes/Characters/Player/player.tscn");
     private Marker2D spawnPlayer;
-    private Button _button;
-    private CharacterBody2D _playerCharacter;
+
+    // variable para el boton
+    private TextureButton _button;
     public override void _Ready()
     {
+        // inicializa los componentes
         _global = GetNode<Global>("/root/Global");
-        _button = GetNode<Button>("Node/Control/Button");
+        _button = GetNode<TextureButton>("Gui/CanvasLayer/Control/TextureButton");
 
-
+        // instancia el nivel y lo agrega al nodo padre
         spawnPlayer = GetNode<Marker2D>("SpawnPlayer");
         AddChild(_global.levels[_global.currentLevel].Instantiate());
 
+        // aumenta el nivel en la variable global
         _global.currentLevel += 1;
 
+        // instancia al jugador y lo agrega al nodo padre
         CharacterBody2D newPlayer = (CharacterBody2D)player.Instantiate();
         newPlayer.Position = spawnPlayer.Position;
         GetParent().AddChild(newPlayer);
-    }
-
-    public override void _Process(double delta)
-    {
-        if(_button.ButtonPressed == true)
-        {
-            _playerCharacter = GetNode<CharacterBody2D>("/root/Player");
-            _playerCharacter.QueueFree();
-        }
     }
 }
--- Scripts/LevelButton.cs
+++ Scripts/LevelButton.cs
@@ -3,14 +3,17 @@ using System;
 
 public partial class LevelButton : TextureButton
 {
-
+    // variable global
     private Global _global;
+
+    // variable para el id del nivel
     [Export] public int levelId;
     public override void _Ready()
     {
+        // inicializa los componentes y verifica si el nivel esta desbloqueado
         _global = GetNode<Global>("/root/Global");
 
-        if(levelId == _global.currentLevel)
+        if(levelId >= _global.currentLevel)
         {
             Disabled = false;
         }
@@ -23,6 +26,7 @@ public partial class LevelButton : TextureButton
 
     public void OnButtonPressed()
     {
+        // al presionar el boton se cambia a la escena game y se asigna el id del nivel que sera instanciado
         _global.levelId = levelId;
         GetTree().ChangeSceneToFile("res://Scenes/Levels/game.tscn");
         
